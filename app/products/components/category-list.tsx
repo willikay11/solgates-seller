eae1308ddbff2a1325df84de7a21ad1a091a2e19
William Kamau <william.kamuyu@gapstack.com>
2026-02-05 17:15:34 +0300

    fix: update CategoryList component to use expo-checkbox and remove RadioButton

--- app/products/components/category-list.tsx
+++ app/products/components/category-list.tsx
@@ -1,5 +1,4 @@
 import { StyleSheet, View, Text, ActivityIndicator, TouchableWithoutFeedback, Keyboard, KeyboardEvent } from "react-native";
-import { RadioButton } from "react-native-paper";
 import Checkbox from 'expo-checkbox';
 import { useEffect } from "react";
 
@@ -43,7 +42,7 @@ export default function CategoryList({
     return (
         <View style={styles.columnContainer}>
             <Text style={styles.filterTitle}>{title}</Text>
-            <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', marginHorizontal: 10, }}>
                 {isLoading ? (
                     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', paddingVertical: 20 }}>
                         <ActivityIndicator size="small" color="#EA580C" />
@@ -53,32 +52,42 @@ export default function CategoryList({
                         <View style={styles.checkboxContainer} key={`${item.id}-${index}`}>
                             {multiple ? (
                                 <TouchableWithoutFeedback onPress={() => toggleCheck({ id: item.id, label: item.label })}>
-                                    <View style={{ flexDirection: 'row', alignItems: 'center', height: 40 }}>
+                                    <View style={{ flexDirection: 'row', alignItems: 'center', height: 30 }}>
                                     <Checkbox
                                         value={
                                         typeof checkedItems === 'string'
                                             ? checkedItems === item.id
                                             : checkedItems?.[item.id] || false
                                         }
-                                        color="#EA580C"
+                                        color={typeof checkedItems === 'string'
+                                            ? checkedItems === item.id
+                                                ? '#EA580C'
+                                                : '#1F2937'
+                                            : checkedItems?.[item.id]
+                                                ? '#EA580C'
+                                                : '#1F2937'
+                                        }
                                         onValueChange={() => toggleCheck({ id: item.id, label: item.label })}
                                     />
-                                    <Text style={{ fontSize: 14 }}>{item.label}</Text>
+                                    <Text style={{ fontSize: 14, marginLeft: 10 }}>{item.label}</Text>
                                     </View>
                                 </TouchableWithoutFeedback>
                             ) : (
                                 <TouchableWithoutFeedback onPress={() => toggleCheck({ id: item.id, label: item.label })}>
-                                    <View style={{ flexDirection: 'row', alignItems: 'center', height: 40 }}>
-                                    <RadioButton
-                                        value={item.id}
-                                        status={
-                                        typeof checkedItems === 'string'
-                                            ? checkedItems === item.id ? 'checked' : 'unchecked'
-                                            : checkedItems?.[item.id] ? 'checked' : 'unchecked'
-                                        }
-                                        onPress={() => toggleCheck({ id: item.id, label: item.label })}
-                                        color="#EA580C"
-                                    />
+                                    <View style={{ flexDirection: 'row', alignItems: 'center', height: 30 }}>
+                                    <View style={[
+                                        styles.radioButton,
+                                        { borderColor: (typeof checkedItems === 'string'
+                                            ? checkedItems === item.id
+                                            : !!checkedItems?.[item.id]) ? '#EA580C' : '#1F2937' }
+                                    ]}>
+                                        <View style={[
+                                            styles.radioButtonInner,
+                                            (typeof checkedItems === 'string'
+                                                ? checkedItems === item.id
+                                                : !!checkedItems?.[item.id]) && styles.radioButtonInnerChecked
+                                        ]} />
+                                    </View>
                                     <Text style={{ fontSize: 14 }}>{item.label}</Text>
                                     </View>
                                 </TouchableWithoutFeedback>
@@ -111,5 +120,26 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         gap: 10,
+    },
+    radioButton: {
+        width: 20,
+        height: 20,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#1F2937',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 8,
+        overflow: 'hidden',
+    },
+    radioButtonInner: {
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        backgroundColor: 'transparent',
+        overflow: 'hidden',
+    },
+    radioButtonInnerChecked: {
+        backgroundColor: '#EA580C',
     },
 })
